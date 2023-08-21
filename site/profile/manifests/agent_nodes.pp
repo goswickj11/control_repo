257c7fb5f667fcd8dc6d775a90fc17100834cf49
goswickj11 <142737135+goswickj11@users.noreply.github.com>
2023-08-21 09:17:19 -0500

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -3,12 +3,6 @@ class profile::agent_nodes {
   dockeragent::node {'web.puppet.vm':}
   dockeragent::node {'db.puppet.vm':}
 
-include dockeragent
 
-# This will manage 5 containerized agents.
-range(1,5).each |$n| {
-    dockeragent::node { "agent${n}.${::fqdn}":
-        ports => ["${n}0080:80"],
-    }
 }
 }
